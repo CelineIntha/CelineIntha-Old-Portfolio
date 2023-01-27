65ea5ad5d72ab13eb6c643871822528a7cd045c4
Céline Inthaaphay <celine.inthaaphay@stagiairesmns.fr>
2023-01-27 20:20:05 +0100

    Modification fleche

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
 
-function ShowAndHide() {
-    var x = document.getElementById('SectionName');
-    if (x.style.display == 'none') {
-        x.style.display = 'block'
-    } else {
-        x.style.display = 'none'
-    }
-}
-
 // Pour les 2 boutons
 
 var btn = document.getElementById('bouton')
