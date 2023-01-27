6cbc18ffe474560d2e72f0e9f840d6bda99d5b7d
Céline Inthaaphay <celine.inthaaphay@stagiairesmns.fr>
2023-01-27 19:42:27 +0100

    Ajout d'un bouton switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,15 @@ function ShowAndHide() {
         x.style.display = 'none'
     }
 }
+
+// Pour les 2 boutons
+
+var btn = document.getElementById('bouton')
+
+function leftClick() {
+    btn.style.left = '0'
+}
+
+function rightClick() {
+    btn.style.left = '50%'
+}
