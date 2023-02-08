b0b99a1458be4f9e39413907aaaf204b3fa894b9
Céline <118277079+CelineIntha@users.noreply.github.com>
2023-02-08 15:33:55 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,24 @@ function showNav() {
     x.style.display = "block";
   }
 }
+
+
+// Pour le bouton scroll to top
+let mybutton = document.getElementById("ButtonTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
